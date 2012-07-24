f1afd1625fc7e3a88c81b03906df95bfbf3b431f
Amanda Hoisington <ahoising@bloc609-06.(none)>
2012-07-24 06:15:44 -0500

    Fix bugs, model might be working now

--- bearpop.py
+++ bearpop.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 n = 10
-m = 10
-v = 0.1
-N = 100
+m = 5
+v = 0.01
+N = 1000
 
-bearmaxinit=5
+# fundamental function of model, compute probability of move to new square
+def Pmove(Nfrom, Nto, Bmap):
+  return v * (Nfrom - Nto) * H(Nfrom - Nto)
+
+bearmaxinit=2
 # return list of tuples of initial bear locations
 def initialize():
   def flatten(l):
@@ -15,7 +19,7 @@ def initialize():
       return []
     else:
       return flatten(l[0]) + flatten(l[1:])
-  return flatten([[[(i,j) for num in range(np.random.random_integers(bearmaxinit))] for j in range(m)] for i in range(n)])
+  return flatten([[[(i,j) for num in range(np.random.random_integers(bearmaxinit+1)-1)] for j in range(m)] for i in range(n)])
 
 def process(bears, k):
   print k
@@ -35,18 +39,24 @@ def main():
     R.append(0)
     for bi in range(len(bears)):
       Pu = Pd = Pl = Pr = 0
-      if bears[k][0]>0:
-        Pl = Pmove(Bmap[bears[k]], Bmap[ bears[k][0] - 1, bears[k][1] ], Bmap)
-      if bears[k][0]<m-1:
-        Pr = Pmove(Bmap[bears[k]], Bmap[ bears[k][0] + 1, bears[k][1] ], Bmap)
-      if bears[k][1]>0:
-        Pu = Pmove(Bmap[bears[k]], Bmap[ bears[k][0], bears[k][1] - 1 ], Bmap)
-      if bears[k][1]<n-1:
-        Pd = Pmove(Bmap[bears[k]], Bmap[ bears[k][0], bears[k][1] + 1 ], Bmap)
+      if bears[bi][0]>0:
+        Pu = Pmove(Bmap[bears[bi]], Bmap[ bears[bi][0] - 1, bears[bi][1] ], Bmap)
+      if bears[bi][0]<m-1:
+        Pd = Pmove(Bmap[bears[bi]], Bmap[ bears[bi][0] + 1, bears[bi][1] ], Bmap)
+      if bears[bi][1]>0:
+        Pl = Pmove(Bmap[bears[bi]], Bmap[ bears[bi][0], bears[bi][1] - 1 ], Bmap)
+      if bears[bi][1]<n-1:
+        Pr = Pmove(Bmap[bears[bi]], Bmap[ bears[bi][0], bears[bi][1] + 1 ], Bmap)
 
       R[k] += Pl + Pr + Pu + Pd
 
-      move = choose([Pl, Pr, Pu, Pd])
+      move = choose([Pu, Pd, Pl, Pr])
+
+      assert Pu > 0 or move != 0
+      assert Pd > 0 or move != 1
+      assert Pl > 0 or move != 2
+      assert Pr > 0 or move != 3
+
       if move == 0:
         Bmap[bears[bi]] -= 1
         bears[bi] = (bears[bi][0] - 1, bears[bi][1])
@@ -57,20 +67,21 @@ def main():
         Bmap[bears[bi]] += 1
       elif move == 2:
         Bmap[bears[bi]] -= 1
-        bears[bi] = (bears[bi][0], bears[bi][1] + 1)
+        bears[bi] = (bears[bi][0], bears[bi][1] - 1)
         Bmap[bears[bi]] += 1
       elif move == 3:
         Bmap[bears[bi]] -= 1
-        bears[bi] = (bears[bi][0], bears[bi][1] - 1)
+        bears[bi] = (bears[bi][0], bears[bi][1] + 1)
         Bmap[bears[bi]] += 1
 
     process(bears, k)
 
   return R
 
-def Pmove(Nfrom, Nto, Bmap):
-  return v * (Nfrom - Nto) * H(Nfrom - Nto)
 
+# Utility functions below
+
+# Heaviside function
 def H(x):
   if x >= 0:
     return 1
@@ -86,6 +97,11 @@ def choose(Plist):
       return i
     choice -= Plist[i]
   return -1
+
+def prettyprint(bears):
+  for i in range(m):
+    pass
+
 
 if __name__ == "__main__":
   main()
